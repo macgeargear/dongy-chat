f9f24dd66c0191cbcc642765537222d62e632f2e
D33102 <dalai2547@gmail.com>
2025-03-24 23:36:24 +0700

    chore: remove role

--- server/src/utils/middleware.ts
+++ server/src/utils/middleware.ts
@@ -8,10 +8,8 @@ export async function requireAuth(req: FastifyRequest, reply: FastifyReply) {
     const user = req.server.jwt.verify<{
       id: string;
       username: string;
-      role: string;
     }>(token);
 
-    // Optional: attach user to the request
     (req as any).user = user;
   } catch {
     return reply.status(401).send({ error: "Invalid token" });
