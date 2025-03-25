484fa029f6e36fce1c17b97e0f45f17cf75d8d28
Vorraphan Treepoonsap <vorraphant@gmail.com>
2025-03-25 21:44:36 +0700

    feat: init channel api

---
+++ server/src/routes/channel.ts
@@ -0,0 +1,8 @@
+// **Channel**
+// - **GET /api/channels**: Retrieve a list of all channels.
+// - **GET /api/channels/:id**: Retrieve a specific channel by ID.
+// - **POST /api/channels**: Create a new channel.
+// - **PUT /api/channels/:id**: Update a channel's information.
+// - **DELETE /api/channels/:id**: Delete a channel.
+// - **POST /api/channels/user/:user_id**: Add a user to a channel.
+// - **DELETE /api/channels/uesr/:user_id**: Remove a user from a channel.
