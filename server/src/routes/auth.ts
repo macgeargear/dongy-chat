176c733a53707bd3511a6c3c771d18f9a18b471c
Gear <macgeargear@gmail.com>
2025-03-26 21:43:07 +0700

    feat: add image url

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -21,6 +21,13 @@ const authRoutes: FastifyPluginAsync = async (app) => {
       data: { username, password: hashed, displayName },
     });
 
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        imageUrl: `https://api.dicebear.com/9.x/notionists-neutral/svg?seed=${user.id}`,
+      },
+    });
+
     const token = app.jwt.sign({
       id: user.id,
       username: user.username,
