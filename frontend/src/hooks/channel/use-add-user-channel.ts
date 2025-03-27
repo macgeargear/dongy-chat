8ee0670c61f0cfeb686730c33ac93a577bf70f2a
Gear <macgeargear@gmail.com>
2025-03-28 01:38:36 +0700

    feat: toast

---
+++ frontend/src/hooks/channel/use-add-user-channel.ts
@@ -0,0 +1,30 @@
+import api from "@/lib/axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export function useAddUserChannel() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({
+      userId,
+      channelId,
+    }: {
+      userId: string;
+      channelId: string;
+    }) => {
+      const res = await api.post("/api/channel-member", {
+        userId,
+        channelId,
+      });
+
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["channels"] });
+    },
+    onError: () => {
+      toast.error("Failed to add user to channel");
+    },
+  });
+}
